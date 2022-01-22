d109c93b1d0ce297a1c463b886972ddb3111d047
Kushagra Rajpoot <kushagra305@gmail.com>
2022-01-22 22:25:11 +0530

    NQueen

---
+++ NQueen.js
@@ -0,0 +1,40 @@
+function NQueen(A){
+    let vigRow = new Array(A).fill(0);
+    let vigCol = new Array(A).fill(0);
+    let d1 = new Array(2*A - 1).fill(0);
+    let d2 = new Array(2*A - 1).fill(0);
+    let final = [];
+
+    function generate(set, idx) {
+        if(idx === A) {
+            final.push([...set]);
+            return;
+        }
+
+        for(let i = 0; i < A; i++) {
+            if(vigRow[idx] !== 1 
+                && vigCol[i] !== 1 
+                && d1[i+idx] !== 1 
+                && d2[A - 1 + i - idx] !== 1) {
+                    vigRow[idx] = 1;
+                    vigCol[i] = 1;
+                    d1[i+idx] = 1;
+                    d2[A - 1 + i - idx] = 1;
+                    set.push([idx, i]);
+                    generate(set, idx + 1);
+                    set.pop();
+                    vigRow[idx] = 0;
+                    vigCol[i] = 0;
+                    d1[i+idx] = 0;
+                    d2[A - 1 + i - idx] = 0;
+            }
+        }
+    }
+
+    generate([], 0);
+
+    console.log(final);
+   
+
+  
+}
