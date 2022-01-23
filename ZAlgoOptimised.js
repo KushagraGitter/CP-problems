010c529111577d1ba6b1a37e8ccfc4867ed9a163
Kushagra Rajpoot <kushagra305@gmail.com>
2022-01-23 15:33:05 +0530

    Z Algo optimised version with O(n) timer complexity

---
+++ ZAlgoOptimised.js
@@ -0,0 +1,35 @@
+//aabcaabcdaabcaabca
+function ZAlgo(str) {
+    let len = str.length;
+    let zArray  = new Array(len);
+    let l,r = 0;
+    zArray[0] = str.length;
+
+    for(let i = 1; i < len; i++) {
+        if(i > r) {
+            l = i,r = i
+            while(r < len && str[r] === str[r-l]) {
+                r++;
+            }
+            zArray[i] = r-l;
+            r--;
+        }
+        else {
+            let j = i - l;
+            if(str[j] + j <= r) {
+                z[i] = z[i-l];
+            } else {
+                l = i,r = i
+                while(r < len && str[r] === str[r-l]) {
+                    r++;
+                }
+                zArray[i] = r-l;
+                r--;
+            }
+        }
+    }
+    return zArray;
+}
+
+//to execute call function ZAlgo('aabcaabcdaabcaabca')
+
