3a80c26ba69b550a522b039f37a436f14676335d
Kushagra Rajpoot <kushagra305@gmail.com>
2022-03-05 02:33:09 +0530

    link list problem code

---
+++ LinkList/starter.js
@@ -0,0 +1,10 @@
+function Node(data){
+    this.data = data
+    this.next = null   
+}
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
