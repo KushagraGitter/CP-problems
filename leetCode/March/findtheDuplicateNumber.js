5a47728551a8053fd532171959c5e3088f2eaf83
KushagraGitter <kushagra305@gmail.com>
2022-03-29 21:15:25 +0530

    1 file added

---
+++ leetCode/March/findtheDuplicateNumber.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  let ans = 0;
+  function getSmallerNumberCount(num) {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] <= num) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  function bs(start, end) {
+    if (start > end) {
+      return -1;
+    }
+
+    const mid = Math.floor((start + end) / 2);
+
+    let count = getSmallerNumberCount(mid);
+
+    if (count > mid) {
+      ans = mid;
+      return bs(start, mid - 1);
+    } else {
+      return bs(mid + 1, end);
+    }
+  }
+  bs(1, nums.length);
+  return ans;
+};
