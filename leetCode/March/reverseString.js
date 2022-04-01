7c2a9c94b5ec7a82633d3f47a3646bde5a90889d
KushagraGitter <kushagra305@gmail.com>
2022-04-01 17:35:32 +0530

    1 file added

---
+++ leetCode/March/reverseString.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+  for(let i = 0; i < s.length / 2; i++) {
+      const temp = s[s.length - 1 - i] ;
+      s[s.length - 1 - i] = s[i];
+      s[i] = temp
+  }
+  
+  return s
+};
