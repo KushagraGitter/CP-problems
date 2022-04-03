3c6e9138cb203285e41a34b20537a83fb1ab4542
KushagraGitter <kushagra305@gmail.com>
2022-04-04 03:19:50 +0530

    1 file modified

--- Arrays/ARRAYS & DYNAMIC ARRAYS/waveArray.js
+++ Arrays/ARRAYS & DYNAMIC ARRAYS/waveArray.js
@@ -1,29 +1,26 @@
 function waveArray(A) {
-
   const sortedA = A.sort((a, b) => a - b);
   let leftLargeCheck = true;
 
-  for(let i=0; i<A.length; i++) {
-  
-      if(leftLargeCheck) {
-         if(A[i] < A[i+1]) {
-             let temp;
-             temp = A[i+1];
-             A[i+1] = A[i];
-             A[i] = temp;
-         }
-         leftLargeCheck = false;
-
-      } else {
-         if(A[i] > A[i+1]) {
-             let temp;
-             temp = A[i+1];
-             A[i+1] = A[i];
-             A[i] = temp;
-         }
-         leftLargeCheck = true; 
+  for (let i = 0; i < A.length; i++) {
+    if (leftLargeCheck) {
+      if (A[i] < A[i + 1]) {
+        let temp;
+        temp = A[i + 1];
+        A[i + 1] = A[i];
+        A[i] = temp;
       }
-
+      leftLargeCheck = false;
+    } else {
+      if (A[i] > A[i + 1]) {
+        let temp;
+        temp = A[i + 1];
+        A[i + 1] = A[i];
+        A[i] = temp;
+      }
+      leftLargeCheck = true;
+    }
   }
   return A;
+  
 }
