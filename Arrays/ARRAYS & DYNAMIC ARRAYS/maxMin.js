ea096805e8d0404ade119b43e9ebd58060fe8a4a
KushagraGitter <kushagra305@gmail.com>
2022-04-06 21:36:28 +0530

    1 file modified, 1 added

--- Arrays/ARRAYS & DYNAMIC ARRAYS/maxMin.js
+++ Arrays/ARRAYS & DYNAMIC ARRAYS/maxMin.js
@@ -1,21 +1,20 @@
-module.exports = { 
+module.exports = {
   //param A : array of integers
   //param B : integer
   //return an integer
-   solve : function(A, B){
-         A = A.sort((a, b) => a - b);
- 
-         let bMinEl = 0;
-         let bMaxEl = 0;
-         let left  = 0;
-         let right = A.length - 1;
-     
-         while(!(left + 1 === B)) {
-             left++;
-             right--;
-         }
- 
-         return A[right] - A[left];
-   }
- };
- 
+  solve: function (A, B) {
+    A = A.sort((a, b) => a - b);
+
+    let bMinEl = 0;
+    let bMaxEl = 0;
+    let left = 0;
+    let right = A.length - 1;
+
+    while (!(left + 1 === B)) {
+      left++;
+      right--;
+    }
+
+    return A[right] - A[left];
+  },
+};
