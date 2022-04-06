7d649b5825eeda62d00cca6ba4f3330b3876f790
KushagraGitter <kushagra305@gmail.com>
2022-04-06 21:19:15 +0530

    1 file modified

--- leetCode/April/3SumWithMultiplicity.js
+++ leetCode/April/3SumWithMultiplicity.js
@@ -3,49 +3,47 @@
  * @param {number} target
  * @return {number}
  */
- var threeSumMulti = function(arr, target) {
+var threeSumMulti = function (arr, target) {
   // we can use two pointers to solve the question for pair, if we break the poblem and for i we can find {j, k}
   let retArr = [];
   let ans = 0;
   let mod = 1000000007;
   arr = arr.sort((a, b) => a - b);
-  
-  for(let i = 0; i < arr.length; i++) {
-      
-      let j = i + 1;
-      let k = arr.length - 1;
-      const t = target - arr[i];
-      
-      while(j < k) {
-          if(arr[j] + arr[k] < t) {
-              j++;
-          } else if(arr[j] + arr[k] > t) {
-              k--;
-          } else if(arr[j] !== arr[k]) {
-              let left  = 1; let right = 1;
-              
-              while(j + 1 < k && arr[j] === arr[j + 1]) {
-                  j++;
-                  left++;
-              }
 
-              while(k - 1 >= 0 && arr[k - 1] === arr[k]) {
-                  k--;
-                  right++;
-              }
+  for (let i = 0; i < arr.length; i++) {
+    let j = i + 1;
+    let k = arr.length - 1;
+    const t = target - arr[i];
 
-              ans = ans + left * right;
-              ans = ans%mod;
-              j++;
-              k--;
-          }
+    while (j < k) {
+      if (arr[j] + arr[k] < t) {
+        j++;
+      } else if (arr[j] + arr[k] > t) {
+        k--;
+      } else if (arr[j] !== arr[k]) {
+        let left = 1;
+        let right = 1;
 
-          else {
-              ans = ans + (k-j+1)*(k-j) / 2;
-              ans = ans%mod;
-              break;
-          }
+        while (j + 1 < k && arr[j] === arr[j + 1]) {
+          j++;
+          left++;
+        }
+
+        while (k - 1 >= 0 && arr[k - 1] === arr[k]) {
+          k--;
+          right++;
+        }
+
+        ans = ans + left * right;
+        ans = ans % mod;
+        j++;
+        k--;
+      } else {
+        ans = ans + ((k - j + 1) * (k - j)) / 2;
+        ans = ans % mod;
+        break;
       }
+    }
   }
 
   return ans;
