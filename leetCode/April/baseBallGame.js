04c42e4ecce347ea840ba00ff65e902323e98e12
KushagraGitter <kushagra305@gmail.com>
2022-04-10 12:41:04 +0530

    1 file added

---
+++ leetCode/April/baseBallGame.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} ops
+ * @return {number}
+ */
+var calPoints = function (ops) {
+  let arr = [];
+
+  for (let i = 0; i < ops.length; i++) {
+    if (ops[i] === 'C') {
+      arr.pop();
+    } else if (ops[i] === 'D') {
+      arr.push(arr[arr.length - 1] * 2);
+    } else if (ops[i] === '+') {
+      const el1 = arr.pop();
+      const newVal = arr[arr.length - 1] + el1;
+      arr.push(el1);
+      arr.push(newVal);
+    } else {
+      arr.push(parseInt(ops[i]));
+    }
+  }
+
+  let initalSum = 0;
+
+  let sum = arr.reduce((a, b) => a + b, initalSum);
+
+  return sum;
+};
