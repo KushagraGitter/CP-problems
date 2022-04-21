3eb86b6ba32daa60b2e48393564041c5a188278a
KushagraGitter <kushagra305@gmail.com>
2022-04-21 17:05:07 +0530

    1 file modified, 1 added

--- Trees/TreeProblemsHard/serialiseDeSerialise.js
+++ Trees/TreeProblemsHard/serialiseDeSerialise.js
@@ -12,34 +12,33 @@
  * @param {TreeNode} root
  * @return {string}
  */
- var serialize = function(root) {
+var serialize = function (root) {
   // we can use level order traversal to store a tree in form of a string
- if(root == null) 
- {
-     return ''
- }   
- let q = [];
-  let serializeStr = '';  
+  if (root == null) {
+    return '';
+  }
+  let q = [];
+  let serializeStr = '';
   q.push(root);
-    serializeStr = root.val;
-    
-    while(q.length > 0) {
-        const node = q.shift();
-        if(node.left) {
-            q.push(node.left);
-            serializeStr = `${serializeStr},${node.left.val}`;
-        } else {
-            serializeStr = `${serializeStr},$`
-        }
-        if (node.right) {
-            q.push(node.right)
-             serializeStr = `${serializeStr},${node.right.val}`;
-        } else {
-             serializeStr = `${serializeStr},$`
-        }
+  serializeStr = root.val;
+
+  while (q.length > 0) {
+    const node = q.shift();
+    if (node.left) {
+      q.push(node.left);
+      serializeStr = `${serializeStr},${node.left.val}`;
+    } else {
+      serializeStr = `${serializeStr},$`;
     }
-    
-    return serializeStr;
+    if (node.right) {
+      q.push(node.right);
+      serializeStr = `${serializeStr},${node.right.val}`;
+    } else {
+      serializeStr = `${serializeStr},$`;
+    }
+  }
+
+  return serializeStr;
 };
 
 /**
@@ -48,33 +47,33 @@
  * @param {string} data
  * @return {TreeNode}
  */
-var deserialize = function(data) {
-    if(data === '') {
-        return null
+var deserialize = function (data) {
+  if (data === '') {
+    return null;
+  }
+  let q = [];
+
+  let dataArray = data.split(',');
+  let root = new TreeNode(parseInt(dataArray.shift()));
+
+  q.push(root);
+
+  while (q.length > 0 && dataArray.length > 0) {
+    const node = q.shift();
+
+    const l = dataArray.shift();
+
+    if (l !== '$' && l !== undefined) {
+      node.left = new TreeNode(parseInt(l));
+      q.push(node.left);
     }
-    let q = []
-    
-    let dataArray = data.split(',')
-    let root = new TreeNode(parseInt(dataArray.shift()))
-  
-    q.push(root)
 
-    while(q.length > 0 && dataArray.length > 0) {
-      const node = q.shift();
-
-      const l = dataArray.shift();
-      
-        if(l !== '$' && l !== undefined) {
-          node.left = new TreeNode(parseInt(l))
-          q.push(node.left)
-        } 
-
-      const r = dataArray.shift();
-        if(r !== '$' && r !== undefined) {
-          node.right = new TreeNode(parseInt(r))
-          q.push(node.right)
-        } 
+    const r = dataArray.shift();
+    if (r !== '$' && r !== undefined) {
+      node.right = new TreeNode(parseInt(r));
+      q.push(node.right);
     }
+  }
 
   return root;
 };
