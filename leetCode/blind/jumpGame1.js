6ecee77e129ab9c1afed33c0180afe143f9d02c6
KushagraGitter <kushagra305@gmail.com>
2022-04-22 18:38:21 +0530

    1 file modified

--- leetCode/blind/jumpGame1.js
+++ leetCode/blind/jumpGame1.js
@@ -2,19 +2,19 @@
  * @param {number[]} nums
  * @return {boolean}
  */
- var canJump = function(nums) {
-  // we can solve this using greedy sol 
+var canJump = function (nums) {
+  // we can solve this using greedy sol
   // we can check from the goal and shift the goal to start if we can reach the goal from i -1 index.
-  
+
   let goal = nums.length - 1;
-  
-  for(let i = nums.length - 2; i >= 0; i--) {
-      if(i  +  nums[i] >= goal) {
-          goal = i;
-      }
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (i + nums[i] >= goal) {
+      goal = i;
+    }
   }
-  
+
   return goal === 0;
 };
-  
-  //[2, 3, 1, 1, 4]
+
+//[2, 3, 1, 1, 4]
