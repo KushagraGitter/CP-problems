24d2384d4e65308aa136e3a59e405f30451a1c4a
KushagraGitter <kushagra305@gmail.com>
2022-04-25 16:19:49 +0530

    2 files modified

--- mergeTwoSortedLinkedList.js
+++ mergeTwoSortedLinkedList.js
@@ -3,48 +3,56 @@
 //      this.data = data
 //      this.next = null
 //    }
+module.exports = {
+  //param A : head node of linked list
+  //param B : head node of linked list
+  //return the head node in the linked list
+  mergeTwoLists: function (A, B) {
+    let h3 = null,
+      tail = null;
+    if (A === null) {
+      return B;
+    }
+    if (B === null) {
+      return A;
+    }
 
-module.exports = { 
-    //param A : head node of linked list
-    //param B : head node of linked list
-    //return the head node in the linked list
-       mergeTwoLists : function(A, B){
-   
-               let h3 = null, tail = null;
-               if(A === null) { return B };
-               if(B === null) { return A };
-   
-               if(A.data >= B.data) {
-                   h3 = B
-                   B = B.next;
-                   tail = h3;
-                   h3.next = A;
-                   tail = tail.next;
-                   A = A.next;
-               } else {
-                   h3 = A
-                   A = A.next;
-                   tail = h3;
-                   h3.next = B;
-                   tail = tail.next;
-                   B = B.next;
-               }
-   
-               while(A !== null && B!==null) {
-                   if(A.data >= B.data) { append(tail, B) }
-                   else { append(tail, A) }
-               }
-   
-               if(A !== null) { tail.next = A }
-               if(B !== null) { tail.next = B }
-   
-           function append(tail, h) {
-               tail.next = h;
-               tail = tail.next;
-               h = h.next;
-           }
-           return h3;	
-       }
-   
-   };
-   
+    if (A.data >= B.data) {
+      h3 = B;
+      B = B.next;
+      tail = h3;
+      h3.next = A;
+      tail = tail.next;
+      A = A.next;
+    } else {
+      h3 = A;
+      A = A.next;
+      tail = h3;
+      h3.next = B;
+      tail = tail.next;
+      B = B.next;
+    }
+
+    while (A !== null && B !== null) {
+      if (A.data >= B.data) {
+        append(tail, B);
+      } else {
+        append(tail, A);
+      }
+    }
+
+    if (A !== null) {
+      tail.next = A;
+    }
+    if (B !== null) {
+      tail.next = B;
+    }
+
+    function append(tail, h) {
+      tail.next = h;
+      tail = tail.next;
+      h = h.next;
+    }
+    return h3;
+  },
+};
