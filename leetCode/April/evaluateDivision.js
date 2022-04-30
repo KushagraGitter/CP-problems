ff3156779ef2902f9a040f37df021a1b3be9513e
KushagraGitter <kushagra305@gmail.com>
2022-04-30 13:11:59 +0530

    1 file modified, 1 added

---
+++ leetCode/April/evaluateDivision.js
@@ -0,0 +1,51 @@
+//need to recheck after graph class
+//https://leetcode.com/problems/evaluate-division/
+/**
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
+var calcEquation = function (equations, values, queries) {
+  let neighbors = {};
+
+  equations.forEach(([nom, denom], idx) => {
+    const curValue = values[idx];
+    neighbors[nom] = neighbors[nom] || [];
+    neighbors[nom].push([denom, curValue]);
+
+    neighbors[denom] = neighbors[denom] || [];
+    neighbors[denom].push([nom, 1 / curValue]);
+  });
+
+  let result = [];
+  for (let query of queries) {
+    result.push(evaluate(query, neighbors));
+  }
+
+  return result;
+};
+
+function evaluate(query, neighbors) {
+  const [nom, denom] = query;
+  if (!(nom in neighbors) || !(denom in neighbors)) return -1;
+  if (nom === denom) return 1;
+
+  let queue = neighbors[nom].slice();
+  let visited = new Set();
+
+  while (queue.length) {
+    const [variable, value] = queue.shift();
+
+    if (variable === denom) return value;
+    visited.add(variable);
+
+    const next = neighbors[variable];
+    next.forEach(([next, nextValue]) => {
+      if (visited.has(next)) return;
+      queue.push([next, nextValue * value]);
+    });
+  }
+
+  return -1;
+}
--- leetCode/April/isGraphBipartite.js
+++ leetCode/April/isGraphBipartite.js
@@ -3,34 +3,34 @@
  * @return {boolean}
  */
 //need to work on this after graph class
-var isBipartite = function(graph) {
+var isBipartite = function (graph) {
   // create a visited board
   // 0 = unassigned, 1 = group A, 2 = group B
   const visited = new Array(graph.length).fill(0);
   // assigned 0 to group A
   visited[0] = 1;
-  
+
   // assigning group
-  for(let i = 0; i < graph.length; i++) {
-      if(!assignedGroup(i)) {
-          return false;
-      }
+  for (let i = 0; i < graph.length; i++) {
+    if (!assignedGroup(i)) {
+      return false;
+    }
   }
   return true;
   function assignedGroup(start) {
-      for(let i = 0; i < graph[start].length; i++) {
-          const member = graph[start][i];
-          if (visited[member] !== 0 && visited[member] === visited[start]) {
-              return false;
-          }
-          if (visited[member] !== 0) {
-              continue;
-          }
-          visited[member] = visited[start] == 1 ? 2 : 1;
-          if (!assignedGroup(member)) {
-              return false;
-          }
+    for (let i = 0; i < graph[start].length; i++) {
+      const member = graph[start][i];
+      if (visited[member] !== 0 && visited[member] === visited[start]) {
+        return false;
       }
-      return true;
+      if (visited[member] !== 0) {
+        continue;
+      }
+      visited[member] = visited[start] == 1 ? 2 : 1;
+      if (!assignedGroup(member)) {
+        return false;
+      }
+    }
+    return true;
   }
 };
