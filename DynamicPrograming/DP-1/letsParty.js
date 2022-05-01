0d4c59216c59810848078fb178dcb351118df06b
KushagraGitter <kushagra305@gmail.com>
2022-05-02 01:12:08 +0530

    1 file modified, 1 added

---
+++ DynamicPrograming/DP-1/letsParty.js
@@ -0,0 +1,16 @@
+module.exports = { 
+  //param A : integer
+  //return an integer
+   solve : function(A){
+         let dp = new Array(A + 1).fill(0);
+         dp[0] = 1;
+         dp[1] = 1;
+ 
+         for(let i = 2; i <= A; i++) {
+             dp[i] = (((i - 1) * dp[i - 2]) % 10003 + dp[i - 1]) % 10003;
+         }
+ 
+         return dp[A]; 
+   }
+ };
+ 
--- leetCode/may/backspaceStringCompare.js
+++ leetCode/may/backspaceStringCompare.js
@@ -3,52 +3,51 @@
  * @param {string} t
  * @return {boolean}
  */
- var backspaceCompare = function(s, t) {
-  //bf create modify the string by running the loop and check if any backspace charcter is found 
+var backspaceCompare = function (s, t) {
+  //bf create modify the string by running the loop and check if any backspace charcter is found
   //if yes the replace the charcter before the backspace with the char after the backspace.
-  
-  //optimization using two pointer 
-  
+
+  //optimization using two pointer
+
   let i = s.length - 1;
   let j = t.length - 1;
   let skips = 0;
   let skipt = 0;
-  
-  while(i >= 0 || j >= 0) {
-      
-      while( i >= 0) {
-          if(s[i] === '#') {
-              skips++; i--;
-          } else if(skips > 0) {
-              skips--;
-              i--;
-          } else {
-              break;
-          }
+
+  while (i >= 0 || j >= 0) {
+    while (i >= 0) {
+      if (s[i] === '#') {
+        skips++;
+        i--;
+      } else if (skips > 0) {
+        skips--;
+        i--;
+      } else {
+        break;
       }
-      
-      while(j >= 0) {
-          if(t[j] === '#') {
-              skipt++;
-              j--;
-          } else if(skipt > 0) {
-              skipt--;
-              j--;
-          } else {
-              break;
-          }
+    }
+
+    while (j >= 0) {
+      if (t[j] === '#') {
+        skipt++;
+        j--;
+      } else if (skipt > 0) {
+        skipt--;
+        j--;
+      } else {
+        break;
       }
-      
-      if(i >= 0 && j >= 0 && s[i] !== t[j]) {
-          return false;
-      }
-       if ((i >= 0) != (j >= 0))
-              return false;
-      
-      i--; j--;
-      
+    }
+
+    if (i >= 0 && j >= 0 && s[i] !== t[j]) {
+      return false;
+    }
+    if (i >= 0 != j >= 0) return false;
+
+    i--;
+    j--;
   }
-  
+
   return true;
 };
 
