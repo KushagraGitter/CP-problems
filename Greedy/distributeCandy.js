0221318035148484de45027d0bd1089e56a82127
KushagraGitter <kushagra305@gmail.com>
2022-05-03 01:42:40 +0530

    1 file modified

---
+++ Greedy/distributeCandy.js
@@ -0,0 +1,30 @@
+module.exports = { 
+  //param A : array of integers
+  //return an integer
+     candy : function(A){
+    const n = A.length;
+         let leftArray = new Array(n).fill(1);
+         let rightArray = new Array(n).fill(1);
+ 
+         for(let i = 1; i < n ; i++) {
+             if(A[i] > A[i-1]) {
+                 leftArray[i] = leftArray[i - 1] + 1;
+             }
+         }
+ 
+         for(let j = n - 1; j >= 0; j--) {
+             if(A[j + 1] < A[j]) {
+                 rightArray[j] = rightArray[j + 1] + 1;
+             }
+         }
+ 
+         for(let k = 0; k < n; k++) {
+             leftArray[k] = Math.max(leftArray[k], rightArray[k]);
+         }
+         let initialValue = 0;
+         let minValue = leftArray.reduce((a, b) => a + b, initialValue);
+         return minValue;
+ 
+     }
+ };
+ 
--- leetCode/may/sortArrayByParity.js
+++ leetCode/may/sortArrayByParity.js
@@ -2,31 +2,27 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var sortArrayByParity = function(nums) {
+var sortArrayByParity = function (nums) {
   let l = 0;
   let r = nums.length - 1;
-  
-  while(l < r) {
-      
-      if(nums[r] % 2 === 0 && nums[l] % 2 === 0) {
-          l++;
-      }else if(nums[r] % 2 === 0 && nums[l] % 2 !== 0) {
-          //swap
-          const temp = nums[l];
-          nums[l] = nums[r];
-          nums[r] = temp;
-          l++;
-          r--;
-      }else if(nums[r] % 2 !== 0 && nums[l] % 2 !== 0) {
-          r--;
-      }
-      else {
-          l++;
-          r--;
-      }
-      
-      
+
+  while (l < r) {
+    if (nums[r] % 2 === 0 && nums[l] % 2 === 0) {
+      l++;
+    } else if (nums[r] % 2 === 0 && nums[l] % 2 !== 0) {
+      //swap
+      const temp = nums[l];
+      nums[l] = nums[r];
+      nums[r] = temp;
+      l++;
+      r--;
+    } else if (nums[r] % 2 !== 0 && nums[l] % 2 !== 0) {
+      r--;
+    } else {
+      l++;
+      r--;
+    }
   }
-  
+
   return nums;
 };
