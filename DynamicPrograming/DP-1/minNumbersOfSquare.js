f4f3c5458ef20732dd6291ca7d64c240f08341e1
KushagraGitter <kushagra305@gmail.com>
2022-05-05 00:11:39 +0530

    1 file modified

--- DynamicPrograming/DP-1/minNumbersOfSquare.js
+++ DynamicPrograming/DP-1/minNumbersOfSquare.js
@@ -1,19 +1,18 @@
-module.exports = { 
+module.exports = {
   //param A : integer
   //return an integer
-   countMinSquares : function(A){
-         dp = new Array(A + 1).fill(0);
-         for(let i = 1; i <= A; i++) {
-             let ans = Infinity;
- 
-             for(let j = 1; j*j <= i; j++) {
-                 ans = Math.min(ans, dp[i - j*j] + 1)
-             }
- 
-             dp[i] = ans;
-         }
- 
-         return dp[A];
-   }
- };
- 
+  countMinSquares: function (A) {
+    dp = new Array(A + 1).fill(0);
+    for (let i = 1; i <= A; i++) {
+      let ans = Infinity;
+
+      for (let j = 1; j * j <= i; j++) {
+        ans = Math.min(ans, dp[i - j * j] + 1);
+      }
+
+      dp[i] = ans;
+    }
+
+    return dp[A];
+  },
+};
---
+++ leetCode/May/maxNumberOfKSumPairs.js
@@ -0,0 +1,35 @@
+/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function (nums, k) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  nums.sort((a, b) => a - b);
+  let count = 0;
+
+  while (l < r) {
+    if (nums[l] + nums[r] > k) {
+      r--;
+    } else if (nums[l] + nums[r] < k) {
+      l++;
+    } else {
+      l++;
+      r--;
+      count++;
+    }
+  }
+
+  return count;
+};
+
+//TC :- O(nlogn)
+//SC : O(1)
+
+// we can also do the same with extra space with less time using hashMap
+
+//TC :- O(n)
+//SC :- O(n);
