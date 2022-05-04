4d367297f148790b159afde77693c35fe8e27ef3
KushagraGitter <kushagra305@gmail.com>
2022-05-05 02:06:49 +0530

    1 file modified

--- DynamicPrograming/DP-1/maxSubSequenceSum.js
+++ DynamicPrograming/DP-1/maxSubSequenceSum.js
@@ -1,11 +1,11 @@
 function maxSubSequenceSum(A) {
   dp = new Array(A.length).fill(0);
-  
+
   dp[0] = A[0];
   dp[1] = Math.max(A[0], A[1]);
 
-  for(let i = 2; i < A.length; i++) {
-      dp[i] = Math.max(dp[i - 1], A[i] + dp[i - 2]);
+  for (let i = 2; i < A.length; i++) {
+    dp[i] = Math.max(dp[i - 1], A[i] + dp[i - 2]);
   }
 
   return dp[A.length - 1];
