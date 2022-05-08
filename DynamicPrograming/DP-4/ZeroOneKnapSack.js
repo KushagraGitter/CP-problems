6f507f521f02ce383db3eb00adc5b1a40ef03950
KushagraGitter <kushagra305@gmail.com>
2022-05-08 14:47:22 +0530

    1 file modified

--- DynamicPrograming/DP-4/ZeroOneKnapSack.js
+++ DynamicPrograming/DP-4/ZeroOneKnapSack.js
@@ -1,60 +1,57 @@
-module.exports = { 
+module.exports = {
   //param A : array of integers
   //param B : array of integers
   //param C : integer
   //return an integer
-   solve : function(A, B, C){
-         const m = A.length;
- 
-         let dp = new Array(m);
- 
-         for(let i = 0; i <= m; i++) {
-             dp[i] = new Array(C + 1).fill(-1);
-         }
- 
-        //itterative solution  
-         // for(let i = 1; i <= m; i++) {
-         //     for(let j = 1; j <= C; j++) {
-         //         dp[i][j] = dp[i - 1][j];
-         //         if(j >= B[i]) {
-         //             dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - B[i]] + A[i]);
-         //         }
-         //     }
- 
-         // }
- 
-         // return dp[m][C]
- 
-         // const m = A.length;
-         // let dp = new Array(m + 1);
- 
-         // for (let i = 0; i <= m; i++) {
-         //     dp[i] = new Array(C + 1).fill(-1);
-         // }
-       
-         //reccursive solutio
-         function ZeroOneKnapSackRec(i, j) {
-             // base case
-            if (i < 0 || j < 0) {
-                 return 0;
-             }
- 
-             if (dp[i][j] === -1) {
-                 if(j >= B[i]) {
-                     const x = ZeroOneKnapSackRec(i - 1, j);
-                     const y = ZeroOneKnapSackRec(i - 1, j - B[i]) + A[i] 
-                     dp[i][j] = Math.max(x, y);
-                 } else {
-                     dp[i][j] = ZeroOneKnapSackRec(i - 1, j);
-                 }
-             
-             }
- 
-             return dp[i][j];
-         }
- 
-         return ZeroOneKnapSackRec(A.length - 1, C)
- 
-   }
- };
- 
+  solve: function (A, B, C) {
+    const m = A.length;
+
+    let dp = new Array(m);
+
+    for (let i = 0; i <= m; i++) {
+      dp[i] = new Array(C + 1).fill(-1);
+    }
+
+    //itterative solution
+    // for(let i = 1; i <= m; i++) {
+    //     for(let j = 1; j <= C; j++) {
+    //         dp[i][j] = dp[i - 1][j];
+    //         if(j >= B[i]) {
+    //             dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - B[i]] + A[i]);
+    //         }
+    //     }
+
+    // }
+
+    // return dp[m][C]
+
+    // const m = A.length;
+    // let dp = new Array(m + 1);
+
+    // for (let i = 0; i <= m; i++) {
+    //     dp[i] = new Array(C + 1).fill(-1);
+    // }
+
+    //reccursive solution
+    function ZeroOneKnapSackRec(i, j) {
+      // base case
+      if (i < 0 || j < 0) {
+        return 0;
+      }
+
+      if (dp[i][j] === -1) {
+        if (j >= B[i]) {
+          const x = ZeroOneKnapSackRec(i - 1, j);
+          const y = ZeroOneKnapSackRec(i - 1, j - B[i]) + A[i];
+          dp[i][j] = Math.max(x, y);
+        } else {
+          dp[i][j] = ZeroOneKnapSackRec(i - 1, j);
+        }
+      }
+
+      return dp[i][j];
+    }
+
+    return ZeroOneKnapSackRec(A.length - 1, C);
+  },
+};
---
+++ DynamicPrograming/DP-4/zeroNKnapSack.js
@@ -0,0 +1,29 @@
+module.exports = { 
+  //param A : integer
+  //param B : array of integers
+  //param C : array of integers
+  //return an integer
+   solve : function(A, B, C){
+      const m = B.length;
+ 
+         let dp = new Array(m + 1);
+ 
+         for(let i = 0; i <= m; i++) {
+             dp[i] = new Array(A + 1).fill(0);
+         }
+ 
+ 
+         for(let i = 1; i <= m; i++) {
+             for(let j = 1; j <= A; j++) {
+                 dp[i][j] = dp[i - 1][j];
+                 if(j >= C[i]) {
+                     dp[i][j] = Math.max(dp[i][j], dp[i][j - C[i]] + B[i]);
+                 }
+             }
+ 
+         }
+ 
+         return dp[m][A]
+   }
+ };
+ 
