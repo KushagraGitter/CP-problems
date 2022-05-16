422b3d7087ef8a2cf02a44e9e31fb7312923c20c
KushagraGitter <kushagra305@gmail.com>
2022-05-16 12:38:52 +0530

    1 file modified

--- Queue/PerfectNumbers.js
+++ Queue/PerfectNumbers.js
@@ -1,26 +1,25 @@
-module.exports = { 
+module.exports = {
   //param A : integer
   //return an integer
-   solve : function(A){
-         let dp = new Array(A + 1).fill(-1);
-         const mod = Math.pow(10, 9) + 7
- 
-        function waysToSendSignalRec(n) {
-            if(n === 0) {
-                return 1;
-            } else if(n === 1) {
-                return 2;
-            }
- 
-            if(dp[n] === -1) {
-                dp[n] = (waysToSendSignalRec(n - 1) + waysToSendSignalRec(n - 2)) % mod;
-            }
- 
-            return dp[n];
-         }
- 
-         return waysToSendSignalRec(A);
- //dp = dp[i - 1] + dp[i - 2]
-   }
- };
- 
+  solve: function (A) {
+    let dp = new Array(A + 1).fill(-1);
+    const mod = Math.pow(10, 9) + 7;
+
+    function waysToSendSignalRec(n) {
+      if (n === 0) {
+        return 1;
+      } else if (n === 1) {
+        return 2;
+      }
+
+      if (dp[n] === -1) {
+        dp[n] = (waysToSendSignalRec(n - 1) + waysToSendSignalRec(n - 2)) % mod;
+      }
+
+      return dp[n];
+    }
+
+    return waysToSendSignalRec(A);
+    //dp = dp[i - 1] + dp[i - 2]
+  },
+};
