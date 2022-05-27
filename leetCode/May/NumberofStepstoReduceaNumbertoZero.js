9ed80e6d61cc37319baf182cd9d164025e769f93
KushagraGitter <kushagra305@gmail.com>
2022-05-27 07:17:29 +0530

    1 file added

---
+++ leetCode/May/NumberofStepstoReduceaNumbertoZero.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+ var numberOfSteps = function(num) {
+  let count = 0;
+  
+  while(num > 0) {
+      if(num % 2 === 0) {
+          num = num / 2
+      } else {
+          num = num - 1;
+      }
+      
+      count = count + 1;
+  }
+  
+  return count;
+};
