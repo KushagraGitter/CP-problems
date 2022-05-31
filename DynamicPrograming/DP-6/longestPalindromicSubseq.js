d8bb9032798fe66220e6328210eda594ad16379b
KushagraGitter <kushagra305@gmail.com>
2022-05-31 11:56:35 +0530

    1 file modified, 2 added

--- DynamicPrograming/DP-6/longestPalindromicSubseq.js
+++ DynamicPrograming/DP-6/longestPalindromicSubseq.js
@@ -1,37 +1,36 @@
-module.exports = { 
+module.exports = {
   //param A : string
   //return an integer
-   solve : function(A){
-     let B = A.split('').reverse().join('');		
-   let n = A.length;
-     let m = B.length;
- 
-     let dp = new Array(n);
- 
-     for (let i = 0; i < n; i++) {
-       dp[i] = new Array(m).fill(-1);
-     }
- 
-     // reccursive code for dp
- 
-     function lcs(i, j) {
-       if (i === -1 || j === -1) {
-         return 0;
-       }
- 
-       if (dp[i][j] === -1) {
-         if (A[i] === B[j]) {
-           dp[i][j] = 1 + lcs(i - 1, j - 1);
-         } else {
-           dp[i][j] = Math.max(lcs(i, j - 1), lcs(i - 1, j));
-         }
-       }
-       return dp[i][j];
-     }
- 
-     const ans = lcs(n - 1, m - 1);
- 
-     return ans;
-   }
- };
- 
+  solve: function (A) {
+    let B = A.split('').reverse().join('');
+    let n = A.length;
+    let m = B.length;
+
+    let dp = new Array(n);
+
+    for (let i = 0; i < n; i++) {
+      dp[i] = new Array(m).fill(-1);
+    }
+
+    // reccursive code for dp
+
+    function lcs(i, j) {
+      if (i === -1 || j === -1) {
+        return 0;
+      }
+
+      if (dp[i][j] === -1) {
+        if (A[i] === B[j]) {
+          dp[i][j] = 1 + lcs(i - 1, j - 1);
+        } else {
+          dp[i][j] = Math.max(lcs(i, j - 1), lcs(i - 1, j));
+        }
+      }
+      return dp[i][j];
+    }
+
+    const ans = lcs(n - 1, m - 1);
+
+    return ans;
+  },
+};
