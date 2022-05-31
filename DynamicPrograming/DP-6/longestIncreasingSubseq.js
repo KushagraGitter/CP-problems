3ecb1f5314ab22ebe5c726d1e2d00f0a29a6754f
KushagraGitter <kushagra305@gmail.com>
2022-05-31 11:28:54 +0530

    1 file modified

--- DynamicPrograming/DP-6/longestIncreasingSubseq.js
+++ DynamicPrograming/DP-6/longestIncreasingSubseq.js
@@ -1,25 +1,24 @@
-module.exports = { 
+module.exports = {
   //param A : array of integers
   //return an integer
-   lis : function(A){
-     const length = A.length;
-         let dp = new Array(length).fill(0);
- 
-     dp[0] = 1;
- 
-     for(let i = 1; i < length; i++) {
-       let sum = 0;
- 
-       for(let j = i - 1; j >= 0; j--) {
-         if(A[j] < A[i]) {
-           sum = Math.max(sum, dp[j]);
-         }
-       }
- 
-       dp[i] = sum + 1;
-     }
- 
-     return Math.max(...dp)
-   }
- };
- 
+  lis: function (A) {
+    const length = A.length;
+    let dp = new Array(length).fill(0);
+
+    dp[0] = 1;
+
+    for (let i = 1; i < length; i++) {
+      let sum = 0;
+
+      for (let j = i - 1; j >= 0; j--) {
+        if (A[j] < A[i]) {
+          sum = Math.max(sum, dp[j]);
+        }
+      }
+
+      dp[i] = sum + 1;
+    }
+
+    return Math.max(...dp);
+  },
+};
