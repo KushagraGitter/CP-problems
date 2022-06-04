f3e4f505e0bae3d909786dd058742825e1b7af80
KushagraGitter <kushagra305@gmail.com>
2022-06-05 00:51:10 +0530

    1 file modified, 1 added

--- leetCode/June/transposeMatrix.js
+++ leetCode/June/transposeMatrix.js
@@ -3,17 +3,16 @@
  * @param {number[][]} matrix
  * @return {number[][]}
  */
- var transpose = function(A) {
-  if(A.length === 0) return [];
-var w = A[0].length, h = A.length;
-var desArr = [];
-for(var i = 0; i < w; i ++) {
+var transpose = function (A) {
+  if (A.length === 0) return [];
+  var w = A[0].length,
+    h = A.length;
+  var desArr = [];
+  for (var i = 0; i < w; i++) {
     desArr[i] = [];
-    for(var j = 0; j < h; j ++){
-        desArr[i][j] = A[j][i];
+    for (var j = 0; j < h; j++) {
+      desArr[i][j] = A[j][i];
     }
-}
-return desArr;
+  }
+  return desArr;
 };
-
-
