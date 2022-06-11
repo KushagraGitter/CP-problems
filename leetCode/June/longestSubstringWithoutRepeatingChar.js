6df43729ae439c8ed7bd5e1a126bbac980fe723d
KushagraGitter <kushagra305@gmail.com>
2022-06-11 13:31:30 +0530

    1 file modified

--- leetCode/June/longestSubstringWithoutRepeatingChar.js
+++ leetCode/June/longestSubstringWithoutRepeatingChar.js
@@ -1,22 +1,22 @@
-/
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
 /**
  * @param {string} s
  * @return {number}
  */
- var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
   const map = {};
-let left = 0,
+  let left = 0,
     right = 0,
     max_len = 0;
 
-while(right < s.length) {
-    if(s[right] in map && map[s[right]] >= left) {
-        left = map[s[right]] + 1;
+  while (right < s.length) {
+    if (s[right] in map && map[s[right]] >= left) {
+      left = map[s[right]] + 1;
     }
     map[s[right]] = right;
     ++right;
-    
+
     max_len = Math.max(max_len, right - left);
-}
-return max_len;
+  }
+  return max_len;
 };
