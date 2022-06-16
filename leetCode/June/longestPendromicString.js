78645ed155c29096b00c8b50cd51b30982b025d7
KushagraGitter <kushagra305@gmail.com>
2022-06-17 04:29:15 +0530

    1 file added

---
+++ leetCode/June/longestPendromicString.js
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/longest-palindromic-substring/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s;
+  let max = '';
+  for (let i = 0; i < s.length; i++) {
+    let left = helper(i, i, s);
+    let right = helper(i, i + 1, s);
+    let currMax = left.length >= right.length ? left : right;
+    max = currMax.length > max.length ? currMax : max;
+  }
+
+  return max;
+};
+
+function helper(left, right, s) {
+  let curr = '';
+
+  while (left >= 0 && right < s.length && s[left] === s[right]) {
+    curr = s.substring(left, right + 1);
+    left -= 1;
+    right += 1;
+  }
+
+  return curr;
+}
