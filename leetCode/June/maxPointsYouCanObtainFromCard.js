520e3ab98880ee646bc469bac0922b761e604af0
KushagraGitter <kushagra305@gmail.com>
2022-06-27 01:03:35 +0530

    updated

--- leetCode/June/maxPointsYouCanObtainFromCard.js
+++ leetCode/June/maxPointsYouCanObtainFromCard.js
@@ -1,20 +1,22 @@
+https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
 /**
  * @param {number[]} cardPoints
  * @param {number} k
  * @return {number}
  */
-var maxScore = function(cardPoints, k) {
-   let dp = new Array(k + 1).fill(0);
-    
-     for (let i = cardPoints.length - 1; i >= cardPoints.length - k; i--) {
-            dp[0] = dp[0] + cardPoints[i];
-        }
-    
-    let max_points = dp[0];
-    
-      for (let i = 1; i < k + 1; i++) {
-            dp[i] = dp[i - 1] + cardPoints[i - 1] - cardPoints[cardPoints.length - k + i - 1];
-            max_points = Math.max(max_points, dp[i]);
-        }
-    return max_points;
+var maxScore = function (cardPoints, k) {
+  let dp = new Array(k + 1).fill(0);
+
+  for (let i = cardPoints.length - 1; i >= cardPoints.length - k; i--) {
+    dp[0] = dp[0] + cardPoints[i];
+  }
+
+  let max_points = dp[0];
+
+  for (let i = 1; i < k + 1; i++) {
+    dp[i] =
+      dp[i - 1] + cardPoints[i - 1] - cardPoints[cardPoints.length - k + i - 1];
+    max_points = Math.max(max_points, dp[i]);
+  }
+  return max_points;
 };
