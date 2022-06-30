9cf157853c0c6020c4b221db7e30726bd1062e51
KushagraGitter <kushagra305@gmail.com>
2022-07-01 02:12:38 +0530

    leet code jul 1

---
+++ leetCode/June/minOperationsToEqualArrayEl.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var minMoves2 = function (nums) {
+  let operations = 0;
+  nums.sort((a, b) => a - b);
+
+  let mid = Math.floor(nums.length / 2);
+
+  for (let i = 0; i < nums.length; i++) {
+    operations = operations + Math.abs(nums[i] - nums[mid]);
+  }
+
+  return operations;
+};
