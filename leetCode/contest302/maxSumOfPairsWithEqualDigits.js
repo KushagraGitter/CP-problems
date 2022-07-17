4a38454b0f06e25e7b1fdc1ad44220f9d6751f81
KushagraGitter <kushagra305@gmail.com>
2022-07-17 09:53:59 +0530

    1 file added

---
+++ leetCode/contest302/maxSumOfPairsWithEqualDigits.js
@@ -0,0 +1,33 @@
+//https://leetcode.com/contest/weekly-contest-302/problems/max-sum-of-a-pair-with-equal-sum-of-digits/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumSum = function (nums) {
+  let map = new Map()
+  let maxSum = -1
+
+  for (let i = 0; i < nums.length; i++) {
+    const sumD = sumOfDigit(nums[i])
+    if (map.has(sumD)) {
+      let list = map.get(sumD)
+      for (let j = 0; j < list.length; j++) {
+        maxSum = Math.max(maxSum, list[j] + nums[i])
+      }
+      map.get(sumD).push(nums[i])
+    } else {
+      map.set(sumD, [nums[i]])
+    }
+  }
+
+  function sumOfDigit(num) {
+    let sum = 0
+    while (num !== 0) {
+      sum = sum + (num % 10)
+      num = Math.floor(num / 10)
+    }
+    return sum
+  }
+
+  return maxSum
+}
