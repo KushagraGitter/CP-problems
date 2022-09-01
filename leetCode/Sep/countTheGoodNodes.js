dca94b331a1a04f76c1c3f38f644195571afd490
KushagraGitter <kushagra305@gmail.com>
2022-09-01 11:13:48 +0530

    1 file added

---
+++ leetCode/Sep/countTheGoodNodes.js
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function (root) {
+  let goodNodeCount = 0
+
+  function preOrder(node, maxValue) {
+    if (node === null) {
+      return
+    }
+
+    if (node.val >= maxValue) {
+      goodNodeCount++
+    }
+    maxValue = Math.max(maxValue, node.val)
+    // if(node.left) {
+    //   minValue = Math.max(minValue, node.left.val)
+    // }
+    preOrder(node.left, maxValue)
+    // if(node.right) {
+    //   minValue = Math.max(minValue, node.right.val)
+    // }
+    preOrder(node.right, maxValue)
+  }
+
+  preOrder(root, root.val)
+
+  return goodNodeCount
+}
