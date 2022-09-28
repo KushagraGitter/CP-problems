61ab8962e4f7ddff164617b075251628b74edcad
kushagragitter <kushagra305@gmail.com>
2022-09-28 23:29:02 +0530

    first commit from my mac

---
+++ leetCode/Sep/removeNthelementFromLL.js
@@ -0,0 +1,42 @@
+//
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ var removeNthFromEnd = function(head, n) {
+    let tempList = new ListNode(0);
+        tempList.next = head;
+        
+        // set variables for next node and current node
+        let slow = tempList;
+        let fast = tempList;
+        
+        // set fast to n nodes ahead of slow
+        for (let i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        
+        // While we haven't reached the end of the list
+        // set slow to n nodes behind fast
+        while (fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        // set slow.next to two nodes ahead of slow
+        // then return the nth node of the list
+        slow.next = slow.next.next;
+        return tempList.next;
+        
+        
+        
+    };
