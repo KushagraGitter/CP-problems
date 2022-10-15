0b7df1e82aa375540e7a75cf52e1ac1e7190e666
kushagragitter <kushagra305@gmail.com>
2022-10-15 21:29:15 +0530

    edit distance dp leetcode problem

---
+++ leetCode/blind/editDistance.js
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/edit-distance/
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+ var minDistance = function(str1, str2) {
+    let dp = new Array(str1.length + 1).fill(0);
+
+  for(let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(str2.length + 1).fill(0)
+  }
+
+  for(let i = 0; i < dp.length; i++) {
+    dp[i][0] = i 
+  }
+
+  for(let i = 0; i < dp[0].length; i++) {
+    dp[0][i] = i 
+  }
+
+
+  for(let i = 1; i < dp.length; i++) {
+    for(let j = 1; j < dp[0].length; j++) {
+      if(str2[j - 1] !== str1[i - 1])
+        dp[i][j] = 1 + Math.min(Math.min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1])
+      else 
+        dp[i][j] = dp[i - 1][j - 1]
+    }
+  }
+
+  return dp[dp.length - 1][dp[0].length - 1]
+    
+};
