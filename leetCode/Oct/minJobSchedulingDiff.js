e95ab5881436e3b4ad890ca0b0b6ba48c6e76e93
kushagragitter <kushagra305@gmail.com>
2022-10-17 01:37:29 +0530

    leetcode oct [rob sol

---
+++ leetCode/Oct/minJobSchedulingDiff.js
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/
+/**
+ * @param {number[]} jobDifficulty
+ * @param {number} d
+ * @return {number}
+ */
+ var minDifficulty = function(jobDifficulty, d) {
+    if(d > jobDifficulty.length) return -1;   
+    const dp  = new Array(d+1).fill(0).map(()=>new Array(jobDifficulty.length).fill(-1));
+    
+    return partition(jobDifficulty,d,dp,0);
+};
+
+function partition(jobDifficulty,d,dp,idx){
+    if(d===1){
+        let max = 0;
+        while(idx < jobDifficulty.length){
+            max = Math.max(max,jobDifficulty[idx++])
+        }
+        return max;
+    }
+    
+    if(dp[d][idx]!==-1) return dp[d][idx];
+    
+    let max = 0;
+    let res = Number.POSITIVE_INFINITY;
+    
+    for(let i=idx;i<jobDifficulty.length - d + 1;i++){
+        max = Math.max(max,jobDifficulty[i]);
+        res = Math.min(res, max + partition(jobDifficulty,d-1,dp,i+1))
+    }
+    
+    return dp[d][idx] = res
+}
