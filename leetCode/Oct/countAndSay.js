eaf10a1bbef26edbb838593d542b8fc653ec5ee4
kushagragitter <kushagra305@gmail.com>
2022-10-19 01:50:20 +0530

    leetcode problem

---
+++ leetCode/Oct/countAndSay.js
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/count-and-say/
+let curr = "1";
+
+for (let i = 1; i < n; ++i) {
+    let temp = "";
+
+    let cursor = 0;
+    let lastChar = curr[0];
+    let count = 0;
+
+    while (cursor < curr.length) {
+        if (lastChar !== curr[cursor]) {
+            temp += count + lastChar;
+            lastChar = curr[cursor];
+            count = 0;
+        }
+        cursor += 1;
+        count += 1;
+    }
+
+    temp += count + lastChar;
+    curr = temp;
+}
+
+return curr;
