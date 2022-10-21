c53ea7ab6d29e7d6fa33276ac15f496e1d39d207
kushagragitter <kushagra305@gmail.com>
2022-10-21 19:50:33 +0530

    leetcode problem 21 oct

---
+++ leetCode/Oct/findKSortedElements.js
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/find-k-closest-elements/
+/**
+* @param {number[]} arr
+* @param {number} k
+* @param {number} x
+* @return {number[]}
+*/
+var findClosestElements = function(arr, k, x) {
+
+    let lo = 0;
+    let hi = arr.length - 1;
+    while (hi - lo >= k) {
+        if (Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)) {
+            lo++;
+        } else {
+            hi--;
+        }
+    }
+    let result = new Array();
+    for (let i = lo; i <= hi; i++) {
+        result.push(arr[i]);
+    }
+    return result;
+
+};
+
+
