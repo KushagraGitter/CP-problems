e42b936e6944c0ccc26b49f79ea3f82e4eb7bcc9
kushagragitter <kushagra305@gmail.com>
2022-12-03 23:53:55 +0530

    modified with map sorting

--- leetCode/Dec/freqSort.js
+++ leetCode/Dec/freqSort.js
@@ -1,17 +1,21 @@
-//https://leetcode.com/problems/sort-characters-by-frequency/
 /**
-* @param {string} s
-* @return {string}
-*/
+ * @param {string} s
+ * @return {string}
+ */
 var frequencySort = function(s) {
-    const mapChars = s.split('').reduce((acc, ch) => {
-        acc[ch] = (acc[ch] || 0) + 1;
-        return acc;
-        }, {})
-    const sortedChars = Object.entries(mapChars).sort((a, b) => b[1] - a[1]);
-    return sortedChars.reduce((acc, [ch, count]) => {
-        acc += ch.repeat(count);
-        return acc;
-        }, '')
+let map = new Map();
+
+    for(let i = 0; i < s.length; i++) {
+        if(map.has(s[i])) {
+            map.set(s[i], map.get(s[i]) + 1)
+        } else {
+            map.set(s[i], 1)
+        }
+    }
+   const sortedChars = [...map].sort((a, b) => b[1] - a[1]);
+   return sortedChars.reduce((acc, [ch, count]) => {
+       acc += ch.repeat(count);
+       return acc;
+   }, '')
 
 };
