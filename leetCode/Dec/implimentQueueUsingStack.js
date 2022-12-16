e8fb5117c7107df0abd8e4a82537da5d15c1bbcd
kushagragitter <kushagra305@gmail.com>
2022-12-17 00:27:35 +0530

    17 Dec leetcode sol

---
+++ leetCode/Dec/implimentQueueUsingStack.js
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/house-robber/description/
+var MyQueue = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.queue.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.queue.shift()
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.queue[0]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.queue.length > 0 ? false : true
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
