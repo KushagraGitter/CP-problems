9a82ed13d5d9fe643338ff491e3d64a7409891c7
kushagragitter <kushagra305@gmail.com>
2023-02-11 14:32:07 +0530

    with queue implimentation

--- Graphs/GP2/distanceOfNearestCell.js
+++ Graphs/GP2/distanceOfNearestCell.js
@@ -6,25 +6,25 @@ module.exports = {
            let result =  new Array(A.length).fill().map(() => new Array(A[0].length).fill(Infinity));
            let path = [{x:0, y:1}, {x:1, y:0}, {x:0, y:-1}, {x:-1, y:0}];
    
-           let q = [];
+           let q = new Queue();
    
            for(let i = 0; i < A.length; i++) {
                for(let j = 0; j < A[0].length; j++) {
                    if(A[i][j] === 1) {
-                       q.push({i:i, j:j, distance:0})
+                       q.push([i, j , 0])
                        result[i][j] = 0
                        visited[i][j] = 1
                    }
                }
            }
    
-           while(q.length > 0) {
-               const {i, j, distance} = q.shift()
+           while(q.size > 0) {
+               const [i, j, distance] = q.pop()
                for(let k = 0; k < 4; k++) {
                    const newX = i + path[k].x;
                    const newY = j + path[k].y
                    if(newX < A.length && newX > -1 && newY < A[0].length && newY > -1 && visited[newX][newY] === 0) {
-                       q.push({i: newX, j: newY, distance: distance + 1})
+                       q.push([newX, newY,distance + 1])
                        visited[newX][newY] = 1;
                    }
                }
@@ -33,4 +33,26 @@ module.exports = {
            return result
        }
    };
-   
+   class Queue {
+    constructor() {
+        this.elements = {};
+        this.head = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+
+    push(ele) {
+        this.elements[this.tail] = ele;
+        this.tail++;
+        this.size++;
+    }
+
+    pop() {
+        if(this.head === this.tail) return undefined;
+        let ele = this.elements[this.head];
+        delete this.elements[this.head];
+        this.head++;
+        this.size--;
+        return ele;
+    }
+}   
