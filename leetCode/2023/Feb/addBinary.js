03ff30697216399479939e14b56e5a08d2e42051
kushagragitter <kushagra305@gmail.com>
2023-02-14 10:43:10 +0530

    14 Feb leetcode sol

---
+++ leetCode/2023/Feb/addBinary.js
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/add-binary/description/
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    return (BigInt("0b"+a) + BigInt("0b"+b)).toString("2")
+ };
