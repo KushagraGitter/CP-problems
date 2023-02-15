5e24fa736ced4b8b4051b37cdc3a0c5c27f3bec6
kushagragitter <kushagra305@gmail.com>
2023-02-15 12:52:04 +0530

    15 Feb leetcode sol

---
+++ leetCode/2023/Feb/addToArrayFormOFInteger.js
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/add-to-array-form-of-integer/description/
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+var addToArrayForm = function(A, K) {
+    let i = A.length - 1;
+  let carry = 0;
+  while (K || carry) {
+    const d = K % 10;
+    if (i >= 0) {
+      A[i] = A[i] + d + carry;
+      carry = A[i] > 9 ? 1 : 0;
+      A[i] = A[i] % 10;
+      i--;
+    } else {
+      const p = d + carry;
+      carry = p > 9 ? 1 : 0;
+      A.unshift(p % 10);
+    }
+    K = Math.trunc(K / 10);
+  }
+  return A;
+};
