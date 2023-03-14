e27add4648de71faac66b5772095ed8431259df0
kushagragitter <kushagra305@gmail.com>
2023-03-15 00:50:55 +0530

    14 Mar leetcode sol

---
+++ leetCode/2023/March/sumTreePath.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+//https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+var sumNumbers = function(root) {
+    let result = 0
+    dfs(root, "")
+    
+    function dfs(root, currentPath){
+        if(!root) return
+        if(!root.left && !root.right){
+            currentPath += root.val
+            result += parseInt(currentPath)
+            return
+        }
+        dfs(root.left, currentPath + root.val)
+        dfs(root.right, currentPath + root.val)
+    }
+    
+    return result
+};
