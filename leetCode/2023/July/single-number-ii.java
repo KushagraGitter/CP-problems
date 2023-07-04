7210ed0fce4950e377d746eb91addc969596617d
Kushagra Rajpoot <kushagra305@gmail.com>
2023-07-04 18:00:43 +0000

    4 Jul leetcode sol

---
+++ leetCode/2023/July/single-number-ii.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/single-number-ii/
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int x : nums) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        
+        return -1;
+    }
+}
