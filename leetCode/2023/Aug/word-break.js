596913e7808706eb0649acaad62bc29ea2bb74b5
Kushagra Rajpoot <kushagra305@gmail.com>
2023-08-04 14:08:32 +0000

    4 Aug leetcode sol

---
+++ leetCode/2023/Aug/word-break.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+//https://leetcode.com/problems/word-break/description/
+var wordBreak = function(s, wordDict) {
+    let n = s.length;
+    let dp = new Array(n + 1).fill(false);
+    dp[0] = true;
+    let max_len = Math.max(...wordDict.map(word => word.length));
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = i - 1; j >= Math.max(i - max_len - 1, 0); j--) {
+            if (dp[j] && wordDict.includes(s.substring(j, i))) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+
+    return dp[n];
+};
