4acb65ca7f93474f07144fa73626dfb3f23c4896
Kushagra Rajpoot <kushagra305@gmail.com>
2023-08-21 16:28:19 +0000

    21 Aug leetcode sol

---
+++ leetCode/2023/Aug/repeated-substring-pattern.java
@@ -0,0 +1,6 @@
+//https://leetcode.com/problems/repeated-substring-pattern/description/
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        return (s + s).substring(1, s.length() * 2 - 1).contains(s);
+    }
+}
