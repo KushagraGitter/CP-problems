d0b7fe42c8b5850c30c2d5dd907957cba6d39ea4
Kushagra Rajpoot <kushagra305@gmail.com>
2023-09-19 17:55:52 +0000

    19 sep leetcode sol

---
+++ leetCode/2023/Sep/find-the-duplicate-number.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//https://leetcode.com/problems/find-the-duplicate-number/description/?envType=daily-question&envId=2023-09-19
+var findDuplicate = function(nums) {
+    let ans = 0 ;
+    function getSmallerNumberCount(num) {
+        let count = 0;
+        
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] <= num) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    function bs(start, end) {
+        if(start > end) {
+            return -1;
+        }
+        
+        const mid = Math.floor((start + end) / 2);
+        
+        let count = getSmallerNumberCount(mid);
+        
+        if(count > mid) {
+            ans = mid;
+            return bs(start, mid - 1);
+        } else {
+            return bs(mid + 1, end)
+        }
+    }
+    bs(1, nums.length)
+    return ans;
+};
+
+//[1,3,4,2,2]
