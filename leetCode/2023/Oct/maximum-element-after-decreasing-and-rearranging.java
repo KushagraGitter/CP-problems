c15b51e734aac52a328b50f2070640349a2a973d
Kushagra Rajpoot <kushagra305@gmail.com>
2023-11-15 17:41:54 +0000

    15 Nov leetcode sol

---
+++ leetCode/2023/Oct/maximum-element-after-decreasing-and-rearranging.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/?envType=daily-question&envId=2023-11-15
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0] = 1; // first condition
+
+        for (int i = 1; i < arr.length; ++i) {
+            if (Math.abs(arr[i] - arr[i - 1]) <= 1) {
+                continue; // purposely wrote for understanding
+            } else {
+                arr[i] = arr[i - 1] + 1; // second condition
+            }
+        }
+
+        return arr[arr.length - 1]; 
+    }
+}
