a9660159c5e7776cb63013c72f3872f37881f3ea
Kushagra Rajpoot <kushagra305@gmail.com>
2023-12-02 16:13:23 +0000

    2 Dec leetcode sol

---
+++ leetCode/2023/Dec/find-words-that-can-be-formed-by-characters.js
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/?envType=daily-question&envId=2023-12-02
+var countCharacters = function(words, chars) {
+    const counts = new Array(26).fill(0);
+
+    // Step 1: Initialize Character Counts Array
+    for (const ch of chars) {
+        counts[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+
+    let result = 0;
+
+    // Step 3: Check Words
+    for (const word of words) {
+        if (canForm(word, counts)) {
+            // Step 4: Calculate Lengths
+            result += word.length;
+        }
+    }
+
+    return result;
+
+    function canForm(word, counts) {
+        const c = new Array(26).fill(0);
+
+        // Step 2: Update Counts Array
+        for (const ch of word) {
+            const x = ch.charCodeAt(0) - 'a'.charCodeAt(0);
+            c[x]++;
+            if (c[x] > counts[x]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
