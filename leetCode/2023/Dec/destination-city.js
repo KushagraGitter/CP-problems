8f059deaace00285ab540e45fae7b9ed7c1dfbfa
Kushagra Rajpoot <kushagra305@gmail.com>
2023-12-15 13:18:22 +0000

    15 Dec leetcode sol

---
+++ leetCode/2023/Dec/destination-city.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+//https://leetcode.com/problems/destination-city/?envType=daily-question&envId=2023-12-15
+var destCity = function(paths) {
+    const cities = new Set();
+
+        // Collect outgoing cities
+        for (const path of paths) {
+            cities.add(path[0]);
+        }
+
+        // Find destination city with no outgoing path
+        for (const path of paths) {
+            const dest = path[1];
+            if (!cities.has(dest)) {
+                return dest;
+            }
+        }
+
+        return "";
+};
