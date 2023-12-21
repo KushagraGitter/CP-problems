d2bb552173462abcd9a8b4e2f4d2b6593036bdb2
Kushagra Rajpoot <kushagra305@gmail.com>
2023-12-21 20:08:35 +0000

    22 Dec leetcode sol

---
+++ leetCode/2023/Dec/widest-vertical-area-between-two-points-containing-no-points.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/?envType=daily-question&envId=2023-12-21
+var maxWidthOfVerticalArea = function(points) {
+    points.sort((a, b) => a[0] - b[0]);
+
+    let maxWidth = 0;
+
+    for (let i = 1; i < points.length; i++) {
+        let width = points[i][0] - points[i - 1][0];
+        maxWidth = Math.max(maxWidth, width);
+    }
+
+    return maxWidth;
+};
