9089e23d27d02828f016af6f2eeb990ea0f1e642
Kushagra Rajpoot <kushagra305@gmail.com>
2024-01-21 18:20:33 +0000

    21 jan leetcode sol

---
+++ leetCode/2024/jan/house-robber.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//https://leetcode.com/problems/house-robber/?envType=daily-question&envId=2024-01-21
+var rob = function(nums) {
+    if(nums.length == 1) {
+        return nums[0]
+    }
+    if(nums.length == 2) {
+        return Math.max(nums[0], nums[1])
+    }
+    let first = nums[0]
+    let second = Math.max(nums[0], nums[1])
+    let curr = 0
+    for(let i = 2; i < nums.length; i++) {
+        curr = Math.max(second, first + nums[i])
+        first = second
+        second = curr;
+        
+    }
+    
+    return curr
+};
