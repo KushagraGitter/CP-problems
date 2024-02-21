a87888e7d7e096273ccd49ffc0e1f2dfa3da0a9b
Kushagra Rajpoot <kushagra305@gmail.com>
2024-02-21 14:40:08 +0000

    21 Feb leetcode sol

---
+++ leetCode/2024/feb/bitwise-and-of-numbers-range.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+//https://leetcode.com/problems/bitwise-and-of-numbers-range/?envType=daily-question&envId=2024-02-21
+var rangeBitwiseAnd = function(left, right) {
+    let cnt = 0;
+    while (left !== right) {
+        left >>= 1;
+        right >>= 1;
+        cnt++;
+    }
+    return left << cnt;
+};
+
