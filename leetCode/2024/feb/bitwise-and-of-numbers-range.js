758dd1c7539e74872d28a06ced85cdcf877a8172
Kushagra Rajpoot <kushagra305@gmail.com>
2024-03-02 17:22:01 +0000

    update

--- leetCode/2024/feb/bitwise-and-of-numbers-range.js
+++ leetCode/2024/feb/bitwise-and-of-numbers-range.js
@@ -3,6 +3,7 @@
  * @param {number} right
  * @return {number}
  */
+//update
 //https://leetcode.com/problems/bitwise-and-of-numbers-range/?envType=daily-question&envId=2024-02-21
 var rangeBitwiseAnd = function(left, right) {
     let cnt = 0;
--- leetCode/2024/feb/maximum-odd-binary-number.js
+++ leetCode/2024/feb/maximum-odd-binary-number.js
@@ -2,6 +2,7 @@
  * @param {string} s
  * @return {string}
  */
+//https://leetcode.com/problems/maximum-odd-binary-number/?envType=daily-question&envId=2024-03-01
 var maximumOddBinaryNumber = function(s) {
     let chars = s.split('').sort().reverse();
     for (let i = chars.length - 1; i >= 0; i--) {
