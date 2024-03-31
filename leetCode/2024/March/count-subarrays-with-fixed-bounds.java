30dca877e09bb06fd885d9f21769c5101449b9f7
Kushagra Rajpoot <kushagra305@gmail.com>
2024-03-31 14:27:15 +0000

    31 March leetcode sol

---
+++ leetCode/2024/March/count-subarrays-with-fixed-bounds.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/count-subarrays-with-fixed-bounds/?envType=daily-question&envId=2024-03-31
+class Solution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+    long ans = 0;
+    int mini = 0, maxi = 0, c = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] == minK)  mini = i;
+        if (nums[i] == maxK)  maxi = i;
+        if (nums[i] < minK || nums[i] > maxK) {
+            c = i + 1;
+            mini = maxi = i;
+        }
+        if (nums[mini] == minK && nums[maxi] == maxK)
+            ans += Math.min(mini - c, maxi - c) + 1;
+    }
+    return ans;
+}
+}
