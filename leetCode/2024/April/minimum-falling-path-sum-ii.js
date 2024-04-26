c7b0e33399c7526c470485e98efb1bbd5f899f1f
Kushagra Rajpoot <kushagra305@gmail.com>
2024-04-26 18:12:48 +0000

    26 Apr leetcode sol

---
+++ leetCode/2024/April/minimum-falling-path-sum-ii.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+//https://leetcode.com/problems/minimum-falling-path-sum-ii/?envType=daily-question&envId=2024-04-26
+var minFallingPathSum = function(A) {
+    const m = A.length;
+   const n = A[0].length;
+
+   let dp = new Array(m);
+
+ 
+
+   for(let i = 0; i < m; i++) {
+       dp[i] = new Array(n).fill(0);
+   }
+
+   // for(let i = 0; i <= n; i++) {
+   //     dp[i][0] = Math.min(...A[i]);    
+   // } 
+
+   for(let j = 0; j < n; j++) {
+       dp[0][j] = A[0][j];    
+   } 
+
+   for(let i = 1; i < m; i++) {
+       for(let j = 0; j < n; j++) {
+             let minSum = Infinity;    
+           dp[i][j] = A[i][j];
+           
+               for(let k = 0 ; k < n; k++) {
+                   if(k !== j) {
+                       minSum = Math.min(minSum, dp[i - 1][k])
+                   }
+               }   
+               dp[i][j] = dp[i][j] + minSum;
+       }
+
+       
+   }
+
+   return Math.min(...dp[m - 1])
+};
