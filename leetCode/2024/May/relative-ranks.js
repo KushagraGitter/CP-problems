6514a663056815f74453c02a8515f60a87e70a1b
Kushagra Rajpoot <kushagra305@gmail.com>
2024-05-08 13:16:32 +0000

    8 May leetcode sol

---
+++ leetCode/2024/May/relative-ranks.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+//https://leetcode.com/problems/relative-ranks/?envType=daily-question&envId=2024-05-08
+var findRelativeRanks = function (score) {
+    const lenScore = score.length;
+    const answer = new Array(lenScore);
+    const scoreWithIndex = score.map((score, index) => [score, index]);
+
+    // Sort the score in descending order
+    scoreWithIndex.sort((a, b) => b[0] - a[0]);
+
+    const rank = ["Gold Medal", "Silver Medal", "Bronze Medal"];
+
+    // Assign ranks based on sorted score
+    for (let i = 0; i < lenScore; i++) {
+        const index = scoreWithIndex[i][1];
+        if (i < 3) {
+            answer[index] = rank[i];
+        } else {
+            answer[index] = String(i + 1);
+        }
+    }
+
+    return answer;
+};
