42d994918e793586b4ae58c95cb5af441abaa14f
Kushagra Rajpoot <kushagra305@gmail.com>
2024-05-16 16:14:06 +0000

    16 May leetcode sol

---
+++ leetCode/2024/May/evaluate-boolean-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+//https://leetcode.com/problems/evaluate-boolean-binary-tree/?envType=daily-question&envId=2024-05-16
+var evaluateTree = function(root) {
+    function helper(node) {
+        if (node.val === 0 || node.val === 1) {
+            return node.val === 1;
+        } else if (node.val === 2) {
+            return helper(node.left) || helper(node.right);
+        } else if (node.val === 3) {
+            return helper(node.left) && helper(node.right);
+        }
+        return false;
+    }
+    
+    return helper(root);
+};
